d6e10b6a7a32c1bfd93e0dc10ac41c4376e10409
Hamish Huggard <hamishhuggard@gmail.com>
2020-04-16 22:52:26 +1200

    create MedTextDetector class

---
+++ medtext_streams/medtext_detector.py
@@ -0,0 +1,16 @@
+class MedTextDetector:
+
+    def __init__(self):
+        pass
+
+    def train(self, train_data):
+        pass
+
+    def add_instance(self, instance, instance_id):
+        pass
+
+    def add_label(self, label, instance_id):
+        pass
+
+    def add_prediction(self, label, instance_id):
+        pass
