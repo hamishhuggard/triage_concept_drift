fca5c5727d82e2e38ce591ba192c8a992cd897aa
Hamish Huggard <hamishhuggard@gmail.com>
2020-04-21 21:08:57 +1200

    start making unittest

---
+++ medtext_streams/unittest.py
@@ -0,0 +1,18 @@
+
+import unittest
+
+from bow_machine import BOWMachine
+from mimic_generator import MIMIC, MIMIC_NEGATE, MIMIC_UNDERSAMPLE, MIMIC_OVERSAMPLE
+from medtext_detector import MedTextDetector
+
+class TestMedTextStuff(unittest.TestCase):
+
+    def test_cdddm(self):
+        self.assertTrue(True)
+        self.assertEqual(2, 1+1)
+
+    def test_bow_machine(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
