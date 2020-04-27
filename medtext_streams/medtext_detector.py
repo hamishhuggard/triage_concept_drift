140ca7a6f6f5108e97ff1559fbbda56abed3beae
Hamish Huggard <hamishhuggard@gmail.com>
2020-04-27 22:12:05 +1200

    fill in most of MedTextDetector

--- medtext_streams/medtext_detector.py
+++ medtext_streams/medtext_detector.py
@@ -2,6 +2,7 @@ from bow_machine import BOWMachine
 from Tornado import DDM
 from cddm import CDDM
 import unittest
+from collections import defaultdict
 
 class DataStream:
 
@@ -44,120 +45,129 @@ class DataStream:
     def send_drift_signal(self):
         print('Drift detected in', self.name)
 
+
+
 class MedTextDetector:
+
+    '''
+    The order in which label_predictions arrive is the canonical order of these
+    the labels. Each label_prediction is associated with an id, which allows
+    them to be matched up with true_labels, in the event that the latter arrives
+    in a different order than the former.
+    '''
 
     def __init__(
             self,
+
             label_set=[1,2,3,4], # the set of possible labels
-            feature_dd, # feature drift detector
-            label_dd, # label drift detector
+            index_instances=False,
+
+            feature_dd=None, # feature drift detector
+            true_label_dd=None, # true label drift detector
+            pred_label_dd=None, # predicted label drift detector
             concept_dd=None, # concept drift detector
-            real_dd=None, # real drift detector
-            # cddm=False # whether to use CDDM or no
+
+            feature_dd_kwargs={},
+            true_label_dd_kwargs={},
+            pred_label_dd_kwargs={},
+            concept_dd_kwargs={},
         ):
+        '''
+        Args:
+        - label_set: a list of the possible priority labels
+        - index_instances: bool indicating whether each instance is indexed.
+            This is because predicted_labels and true_labels can arrive
+            asynchronously, so indices may be needed to match predicted_labels
+            with true_labels. If set to False, then predicted_labels are assumed
+            to arrive in the same order as true_labels.
+        - feature_dd: class of the feature drift detector
+        - true_label_dd: class of the label drift detector
+        - concept_dd: class of the concept drift detector
+        '''
 
-        self.bm = BOWMachine()
 
+        # Label stream stuff
         self.label_set = label_set
+        self.pred_label_stream_dict = {
+            i:DataStream('Label={i}', label_dd(**pred_label_dd_kwargs))
+            for i in label_set
+        }
 
-        self.label_stream_dict = { i:DataStream('Label={i}', label_dd) for i in label_set }
-        self.prediction_stream_dict = { i:DataStream() for i in label_set }
+        self.true_label_stream_dict = {
+            i:DataStream('Label={i}', label_dd(**true_label_dd_kwargs))
+            for i in label_set
+        }
+        # Label queues for handling label asynchronicities
+        self.true_label_queue = []
+        self.pred_label_queue = []
+        self.model_conf_queue = []
+        self.EXPECT_MODEL_CONF = true_label_dd==CDDM
+        self.label_id_queue = []
 
-        self.label_dd_dict = { i:feature_dd() for i in label_set }
-        self.labels = []
-        self.label_val_dict = { i:[] for i in label_set }
-        self.label_state_dict = { i:[] for i in label_set }
-
-        self.prediction_dd_dict = { i:feature_dd() for i in label_set }
-        self.predictions = []
-        self.prediction_val_dict = { i:[] for i in label_set }
-        self.prediction_state_dict = { i:[] for i in label_set }
-
+        # Feature stream stuff
+        self.bm = BOWMachine()
         self.feature_dd = feature_dd
-        self.feature_dd_dict = {}
-        self.feature_vals = {}
+        self.feature_dd_kwargs = feature_dd_kwargs
+        self.feature_stream_dict = {}
 
-        self.concept_dd = concept_dd
-        self.real_dd = real_dd
+        # Loss stream stuff
+        self.loss_stream = DataStream('Loss', concept_dd(**concept_dd_kwargs))
 
-        # self.concept_detector = None
-        # self.virt_drift_detector = None
+    def update_label_streams(self, stream_dicts, label, conf):
+        for label_i in self.label_set:
+            stream_dicts[label_i].add_value(label==label_i, conf)
 
-        # Each instance/label/prediction has a timestamp (ts)
-        # associated with it.
-        # EDIT: wait this doesn't make sense
-        # self.instance_ts_list = []
-        # self.label_ts_list = []
-        # self.prediction_ts_list = []
+    def add_predicted_label(self, label, id, conf=None):
+
+        if (conf!=None) and (self.EXPECT_MODEL_CONF==False):
+            raise ValueError('Model confidence provided when none expected.')
+        if (conf==None) and (self.EXPECT_MODEL_CONF==True):
+            raise ValueError('No model confidence provided when expected.')
+
+        self.pred_label_queue.append(label)
+        self.model_conf_queue.append(conf)
+        self.label_id_queue.append(id)
+        self.true_label_queue.append(None) # the true_label_queue should be the same length as the pred_label_queue
+
+        self.update_label_streams(self.pred_label_stream_dict, label)
+
+    def add_true_label(self, label, id):
+
+        # Match this true label with its corresponding predicted label
+        # by finding the latter with the same ID.
+        def find_id_index(id):
+            for i, id_i in enumerate(self.label_id_queue):
+                if id_i == id:
+                    return id_i
+            raise ValueError(f'Tried to add true label {label} with ID {id},' + \
+                ' but there is no predicted label with the same ID')
+
+        index = find_id_index(id)
+        self.true_label_queue[index] = label
+
+        # Flush out complete pairs of predicted and true labels
+        while len(self.true_label_queue) > 0:
+            if self.true_label_queue[0] != None:
+                true_label = self.true_label_queue.pop(0)
+                pred_label = self.pred_label_queue.pop(0)
+                model_conf = self.model_conf_queue.pop(0)
+                id = self.label_id_queue.pop(0)
+                loss = true_label == pred_label
+                self.update_label_streams(self.true_label_stream_dict, true_label)
+                self.loss_stream.add_value(loss, model_conf)
+            else:
+                break
 
     def add_training_docs(self, training_docs):
 
-        train_data = self.bm.convert_train_data(strings)
+        train_data = self.bm.convert_train_data(training_docs)
 
         for feature in train_data.columns:
-            this_feature_dd = feature_dd()
+            this_feature_dd = self.feature_dd(**self.feature_dd_kwargs)
             for i in range(len(train_data)):
                 value = train_data.loc[i, feature]
                 this_feature_dd.update(value)
             self.feature_dd_dict[feature] = this_feature_dd
 
-    def add_training_label(self, training_labels):
-        for label_i in training_label:
-            for label in self.label_set:
-                outcome = label_i == label
-                detector = self.label_dd_dict[label_i]
-                self.detect_drift(detector, outcome)
-
-
-    def add_label_value(self, dd_dict, value_dict, state_dict,
-            value_list, new_label, training_mode=False):
-
-        for label in self.label_set:
-            outcome = label_i == label
-            detector = label_dict[label_i]
-            self.detect_drift(detector, outcome)
-
-    def add_training_predictions(self, training_predictions):
-
-        pass
-
-    def add_instance(self, instance, instance_ts=None):
-
-        # if instance_ts == None:
-        #     instance_ts = self.instance_ts_list[-1]+1
-
-        instance_data = self.bm.convert_online(strings)
-        for col in instance_data.columns:
-            self.label_detectors[col].do_something()
-
-    def detect_drift(self, detector, val, prob=None):
-        if prob:
-            warning_status, drift_status = detector.detect(val, prob)
-        else:
-            warning_status, drift_status = detector.detect(val)
-        return warning_status, drift_status
-
-    def detect_concept_drift(self, i):
-        label_i = self.labels[i]
-        prediction_i = self.predictions[i]
-        prediction_status = label_i == predictino_i
-        return self.detect_drift(self.concept_dd, prediction_status)
-
-    def add_label(self, label, instance_ts=None):
-
-        # if instance_ts == None:
-        #     instance_ts = self.label_ts_list[-1]+1
-
-        detector.detect(prediction_status)
-        i = len(self.labels)
-        self.detect_concept_drift()
-
-        pass
-
-    def add_prediction(self, label, prob, instance_ts=None):
-
-        # if instance_ts == None:
-        #     instance_ts = self.prediction_ts_list[-1]+1
-
-
+    def add_doc(self, doc):
         pass
