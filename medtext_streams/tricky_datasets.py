dbaa462947c9cb4eb3de7dfcc433469fc02ff40a
Hamish Huggard <hamishhuggard@gmail.com>
2020-05-03 21:29:38 +1200

    create structure of Bernoulli stream

---
+++ medtext_streams/tricky_datasets.py
@@ -0,0 +1,13 @@
+class Bernoulli:
+
+    def __init__(self, concepts, concept_length=25000, transition_length=500, noise_rate=0.1, random_seed=10):
+        '''
+        Each concept is specified by a triple of values (P(X=1), P(Y=1|X=0), P(Y=0|X=1))
+        '''
+        pass
+
+
+class SquareStream:
+
+    def __init__(self):
+        pass
