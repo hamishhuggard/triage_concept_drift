1f05f85235f31df21064f20cc3967575c8bcf595
Hamish Huggard <hamishhuggard@gmail.com>
2020-05-05 22:02:50 +1200

    finish up BERNOULLI

--- medtext_streams/tricky_datasets.py
+++ medtext_streams/tricky_datasets.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class BERNOULLI:
 
-    def __init__(self, concepts, concept_length=25000, transition_length=500, noise_rate=0.1, random_seed=10):
+    def __init__(self, concepts, concept_length=25000, transition_length=500, random_seed=10):
         '''
         Each concept is specified by a triple of values (P(X=1), P(Y=1|X=0), P(Y=0|X=1))
         '''
@@ -16,7 +16,6 @@ class BERNOULLI:
 
         self.__RANDOM_SEED = random_seed
         random.seed(self.__RANDOM_SEED)
-        self.__NOISE_LOCATIONS = random.sample(range(0, self.__INSTANCES_NUM), int(self.__INSTANCES_NUM * noise_rate))
 
         print("You are going to generate a " + self.get_class_name() + " data stream containing " +
               str(self.__INSTANCES_NUM) + " instances, and " + str(self.__NUM_DRIFTS) + " concept drifts; " + "\n\r" +
@@ -49,10 +48,6 @@ class BERNOULLI:
             ending_index = starting_index + self.__W
             self.__RECORDS[starting_index: ending_index] = transition
 
-        # [3] ADDING NOISE
-        if len(self.__NOISE_LOCATIONS) != 0:
-            self.add_noise()
-
         self.write_to_arff(output_path + ".arff")
 
     def create_record(self, concept):
@@ -62,8 +57,49 @@ class BERNOULLI:
         y = x = np.random.choice(2, p=[1-PY1, PY1])
         return x, y
 
+    def write_to_arff(self, output_path):
+        arff_writer = open(output_path, "w")
+        arff_writer.write("@relation BERNOULLI" + "\n")
+        arff_writer.write("@attribute x {0,1}" + "\n" +
+                          "@attribute y {0,1}" + "\n\n")
+        arff_writer.write("@data" + "\n")
 
-class SquareStream:
+        for i in range(0, len(self.__RECORDS)):
+            arff_writer.write(str(self.__RECORDS[i][0]) + "," +
+                              str(self.__RECORDS[i][1]) + "\n")
+        arff_writer.close()
+        print("You can find the generated files in " + output_path + "!")
 
-    def __init__(self):
-        pass
+
+
+class BERNOULLI_BASIC(BERNOULLI):
+
+    def __init__(self, noise=0.1, PX1=0.5 repeats=1, concept_length=25000, transition_length=500, random_seed=10):
+        c1 = [1-noise, noise]
+        c2 = [noise, 1-noise]
+        concepts = [(PX1, *c1), (PX1, *c2)] * repeats
+        super().__init__(concepts, concept_length, transition_length, random_seed)
+
+class BERNOULLI_FP(BERNOULLI):
+
+    def __init__(self, dPX1=0.1, noise=0.1, repeats=1, concept_length=25000, transition_length=500, random_seed=10):
+        c1 = [1-noise, noise]
+        concepts = [(0.5+dPX1, *c1), (0.5-dPX1, *c1)] * repeats
+        super().__init__(concepts, concept_length, transition_length, random_seed)
+
+
+class BERNOULLI_FN(BERNOULLI):
+
+    def __init__(self, noise=0.1, d_noise=0.1, repeats=1, concept_length=25000, transition_length=500, random_seed=10):
+        dPX1 = noise / (2*d_noise)
+        c1 = [1-noise, noise]
+        c2 = [1-noise-d_noise, noise+d_noise]
+        concepts = [(0.5+dPX1, *c1), (0.5-dPX1, *c2)] * repeats
+        super().__init__(concepts, concept_length, transition_length, random_seed)
+
+
+class BERNOULLI_TRICKY(BERNOULLI):
+
+    def __init__(self, epsilon=0.1, concept_length=25000, transition_length=500, random_seed=10):
+        concepts = [(0.5-epsilon, 0.9, 0.4), (0.5+epsilon, 0.9, 0.4)]
+        super().__init__(concepts, concept_length, transition_length, random_seed)
